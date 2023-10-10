47a33876d551266eb6850183613821e60485ff0b
Kibble White <kibblewhite@live.com>
2023-10-10 01:20:52 +0100

    Adding authentication to WASM client app

--- Program.cs
+++ Program.cs
@@ -2,10 +2,16 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Intercom;
 
-WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddOidcAuthentication(options =>
+{
+    builder.Configuration.Bind("Google", options.ProviderOptions);
+    options.ProviderOptions.DefaultScopes.Add("email");
+});
 
 await builder.Build().RunAsync();
